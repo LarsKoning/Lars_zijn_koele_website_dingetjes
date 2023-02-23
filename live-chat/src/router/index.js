19102a801231f045cb7fee634fe97297d7cc2157
LarsKoning <koninglars04@gmail.com>
2023-02-23 14:45:01 +0100

    beginning of live-chat

---
+++ live-chat/src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Welcome from '../views/Welcome.vue'
+import Chatroom from '../views/Chatroom.vue'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'welcome',
+    component: Welcome
+  },
+  {
+    path: '/chatroom',
+    name: 'chatroom',
+    component: Chatroom
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router

c6568d9d1abe0d7cfc0fa875cdcce0d4cec361ee
LarsKoning <koninglars04@gmail.com>
2023-02-27 17:08:47 +0100

    signup form gemaakt

--- music-gurus/composables/useSignup.js
+++ music-gurus/composables/useSignup.js
@@ -2,9 +2,11 @@ import { projectAuth } from "@/firebase/config"
 import { ref } from "vue"
 
 const error = ref(null)
+const isPending = ref(false)
 
 const signup = async (email, password, displayName) => {
   error.value = null
+  isPending.value = true
 
   try {
     const res = await projectAuth.createUserWithEmailAndPassword(email, password)
@@ -13,7 +15,7 @@ const signup = async (email, password, displayName) => {
     }
     await res.user.updateProfile({ displayName })
     error.value = null
-
+    isPending.value = false
     console.log(res.user)
 
     return res
@@ -22,12 +24,13 @@ const signup = async (email, password, displayName) => {
   }catch(err) {
     console.log(err.message)
     error.value = err.message
+    isPending.value = false
   }
 
 }
 
 const useSignup = () => {
-  return { error, signup }
+  return { error, signup, isPending }
 
 }
 
--- music-gurus/src/views/auth/Signup.vue
+++ music-gurus/src/views/auth/Signup.vue
@@ -1,33 +1,35 @@
 <template>
   <form @submit.prevent="handleSubmit">
-    <h3>Login</h3>
+    <h3>Signup</h3>
+    <input type="text" placeholder="Display name" v-model="displayName">
     <input type="email" placeholder="Email" v-model="email">
     <input type="password" placeholder="Password" v-model="password">
     <div class="error" v-if="error">{{ error }}</div>
-    <button v-if="!isPending">Log in</button>
+    <button v-if="!isPending">Signup</button>
     <button v-if="isPending" disabled>Loading</button>
   </form>
 </template>
 
 <script>
 import { ref } from 'vue';
-import useLogin from '/composables/useLogin';
+import useSignup from '/composables/useSignup'
 
 export default {
   setup() {
-    const { error, login, isPending } = useLogin()
+    const { error, signup, isPending } = useSignup()
 
     const email = ref('')
     const password = ref('')
+    const displayName = ref('')
 
     const handleSubmit = async () => {
-      const res = await login(email.value, password.value)
+      const res = await signup(email.value, password.value, displayName.value)
       if (!error.value) {
-        console.log('user logged in')
+        console.log('user signed up')
       }
     }
 
-    return { email, password, handleSubmit, error, isPending }
+    return { email, password, displayName, isPending, error, handleSubmit }
   }
 }
 </script>

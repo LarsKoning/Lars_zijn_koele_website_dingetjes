4a5c09b8a6a9a24e7fde4e76004ec01e96ded695
LarsKoning <koninglars04@gmail.com>
2023-02-27 16:12:29 +0100

    added an login view

---
+++ music-gurus/src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNKmSLQn9kiyvm8uiNjSrvR-NEnw0oGHs",
+  authDomain: "music-gurus.firebaseapp.com",
+  projectId: "music-gurus",
+  storageBucket: "music-gurus.appspot.com",
+  messagingSenderId: "371337371411",
+  appId: "1:371337371411:web:3408328d13f3bc48e784e6"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, projectAuth, timestamp }

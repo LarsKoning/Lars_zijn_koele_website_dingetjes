29196a652f2457c6d33fba79348bdcb826d2d7b0
LarsKoning <koninglars04@gmail.com>
2023-03-26 17:32:20 +0200

    removed some projects

--- lars-jobs/src/views/AboutView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, mollitia temporibus asperiores debitis quas voluptatibus et nam dolorem laboriosam blanditiis odio, aspernatur a explicabo nesciunt quod. Esse possimus assumenda nisi harum nesciunt neque officiis voluptatum suscipit, optio illo ipsam nobis, vero modi doloremque maxime ea blanditiis minima deserunt quod non!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, mollitia temporibus asperiores debitis quas voluptatibus et nam dolorem laboriosam blanditiis odio, aspernatur a explicabo nesciunt quod. Esse possimus assumenda nisi harum nesciunt neque officiis voluptatum suscipit, optio illo ipsam nobis, vero modi doloremque maxime ea blanditiis minima deserunt quod non!</p>
-  </div>
-</template>
